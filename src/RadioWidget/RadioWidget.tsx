8029413ebdd93f84e0e5f434d80ad39077b294b7
uk-taniyama <uk.taniyama@gmail.com>
2022-10-01 11:41:00 +0900

    init

---
+++ src/RadioWidget/RadioWidget.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import Form from "react-bootstrap/Form";
+
+import { WidgetProps, getUiOptions } from "@rjsf/utils";
+
+const RadioWidget = ({
+  id,
+  schema,
+  options,
+  value,
+  required,
+  disabled,
+  readonly,
+  label,
+  onChange,
+  onBlur,
+  onFocus,
+  uiSchema,
+}: WidgetProps) => {
+  const { enumOptions, enumDisabled } = options;
+  const uiOptions = getUiOptions(uiSchema);
+
+  const _onChange = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) =>
+    onChange(schema.type == "boolean" ? value !== "false" : value);
+  const _onBlur = ({ target: { value } }: React.FocusEvent<HTMLInputElement>) =>
+    onBlur(id, value);
+  const _onFocus = ({
+    target: { value },
+  }: React.FocusEvent<HTMLInputElement>) => onFocus(id, value);
+
+  const inline = Boolean(options && options.inline);
+
+  return (
+    <Form.Group className="mb-0">
+      <Form.Label className="d-block">
+        {uiOptions.title || schema.title || label}
+        {(label || uiOptions.title || schema.title) && required ? "*" : null}
+      </Form.Label>
+      {Array.isArray(enumOptions) &&
+        enumOptions.map((option) => {
+          const itemDisabled =
+            Array.isArray(enumDisabled) &&
+            enumDisabled.indexOf(option.value) !== -1;
+          const checked = option.value == value;
+
+          const radio = (
+            <Form.Check
+              inline={inline}
+              label={option.label}
+              id={`${id}-${option.value}`}
+              key={option.value}
+              name={id}
+              type="radio"
+              disabled={disabled || itemDisabled || readonly}
+              checked={checked}
+              required={required}
+              value={option.value}
+              onChange={_onChange}
+              onBlur={_onBlur}
+              onFocus={_onFocus}
+            />
+          );
+          return radio;
+        })}
+    </Form.Group>
+  );
+};
+
+export default RadioWidget;
---
+++ src/TextareaWidget/TextareaWidget.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+import { getUiOptions, WidgetProps } from "@rjsf/utils";
+import FormControl from "react-bootstrap/FormControl";
+import InputGroup from "react-bootstrap/InputGroup";
+
+type CustomWidgetProps = WidgetProps & {
+  options: any;
+};
+
+const TextareaWidget = ({
+  id,
+  placeholder,
+  value,
+  required,
+  disabled,
+  autofocus,
+  label,
+  readonly,
+  onBlur,
+  onFocus,
+  onChange,
+  options,
+  schema,
+  rawErrors = [],
+  uiSchema,
+}: CustomWidgetProps) => {
+  const uiOptions = getUiOptions(uiSchema);
+  const _onChange = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLTextAreaElement>) =>
+    onChange(value === "" ? options.emptyValue : value);
+  const _onBlur = ({
+    target: { value },
+  }: React.FocusEvent<HTMLTextAreaElement>) => onBlur(id, value);
+  const _onFocus = ({
+    target: { value },
+  }: React.FocusEvent<HTMLTextAreaElement>) => onFocus(id, value);
+
+  return (
+    <>
+      <label htmlFor={id}>
+        {uiOptions.title || schema.title || label}
+        {required && (
+          <span
+            aria-hidden
+            className={rawErrors.length > 0 ? "text-danger ml-1" : "ml-1"}
+          >
+            &thinsp;{"*"}
+          </span>
+        )}
+      </label>
+      <InputGroup>
+        <FormControl
+          id={id}
+          name={id}
+          as="textarea"
+          placeholder={placeholder}
+          disabled={disabled}
+          readOnly={readonly}
+          value={value}
+          required={required}
+          autoFocus={autofocus}
+          rows={options.rows || 5}
+          onChange={_onChange}
+          onBlur={_onBlur}
+          onFocus={_onFocus}
+        />
+      </InputGroup>
+    </>
+  );
+};
+
+export default TextareaWidget;
