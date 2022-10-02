6aa9acf3dfb5afa2715c3c00650558f78dd897bf
uk-taniyama <uk.taniyama@gmail.com>
2022-10-02 14:03:58 +0900

    sync window

--- src/stories/SamplePreview.tsx
+++ src/stories/SamplePreview.tsx
@@ -9,13 +9,30 @@ export interface SamplePreviewProps {
   render: FC<PreviewProps>;
 }
 
+function sendName(name:string) {
+  if (window.parent != null && window.parent!== window) {
+    window.parent.postMessage(name);
+  }
+}
+
+let receiveName: (name:string) => void = () => {};
+
+window.addEventListener("message", (event) => {
+  receiveName(event.data);
+}, false);
+
 const SamplePreview: FC<SamplePreviewProps> = ({ render }) => {
   const [name, setName] = useState("");
+  receiveName = setName;
+  const handleName = (name: string) => () => {
+    setName(name);
+    sendName(name);
+  };
   return (
     <>
       <div id="buttons">
         {sampleNames.map((n) => (
-          <button key={n} onClick={() => setName(n)}>
+          <button key={n} onClick={handleName(n)}>
             {n}
           </button>
         ))}
