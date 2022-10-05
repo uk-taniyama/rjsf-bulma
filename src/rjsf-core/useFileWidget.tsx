8c88e9ebde0234da76f5f06542da4a34c862814d
uk-taniyama <uk.taniyama@gmail.com>
2022-10-05 23:36:21 +0900

    FileWidgetを更新。

--- src/rjsf-core/useFileWidget.tsx
+++ src/rjsf-core/useFileWidget.tsx
@@ -1,5 +1,6 @@
 // copy from @rjsf/core 4adf86a1a633ae25fbeae2f9ca3e419716e5a3d2
-import type { ChangeEvent } from "react";
+// FIXME dropとfile inputを共通化。
+import type { ChangeEvent, DragEvent, UIEvent } from "react";
 import { useCallback, useMemo, useState } from "react";
 
 import type { WidgetProps } from "@rjsf/utils";
@@ -18,6 +19,11 @@ export type FileInfoType = {
   size: number;
   type: string;
 };
+
+export interface FilesInfoProps {
+  filesInfo: FileInfoType[];
+  multiple?: boolean;
+}
 
 function processFile(file: File): Promise<FileInfoType> {
   const { name, size, type } = file;
@@ -51,7 +57,7 @@ function processFiles(files: FileList) {
 
 function extractFileInfo(dataURLs: string[]) {
   return dataURLs
-    .filter((dataURL) => typeof dataURL !== "undefined")
+    .filter((dataURL) => dataURL != null)
     .map((dataURL) => {
       const { blob, name } = dataURItoBlob(dataURL);
       return {
@@ -62,11 +68,31 @@ function extractFileInfo(dataURLs: string[]) {
     });
 }
 
+function handleDragEvent(event: DragEvent, multiple?: boolean) {
+  event.stopPropagation();
+  event.preventDefault();
+  const items = event.dataTransfer.items;
+  if (items.length === 0 || (!multiple && items.length > 1)) {
+    event.dataTransfer.dropEffect = "none";
+    return false;
+  }
+  for (let i = 0; i < items.length; i += 1) {
+    const item = items[i];
+    if (item.kind !== "file") {
+      event.dataTransfer.dropEffect = "none";
+      return false;
+    }
+  }
+  event.dataTransfer.dropEffect = "copy";
+  return true;
+}
+
 export function useFileWidget<T, F>({
   value,
   multiple,
   onChange,
 }: WidgetProps<T, F>) {
+  const [dragOver, setDragOver] = useState(false);
   const extractedFilesInfo = useMemo(
     () =>
       Array.isArray(value) ? extractFileInfo(value) : extractFileInfo([value]),
@@ -92,8 +118,64 @@ export function useFileWidget<T, F>({
     },
     [multiple, onChange]
   );
+
+  const handleClear = useCallback(
+    (e?: UIEvent) => {
+      e?.stopPropagation();
+      setFilesInfo([]);
+      onChange(multiple ? [] : undefined);
+    },
+    [multiple, onChange]
+  );
+
+  const handleDragEnter = useCallback(
+    (event: DragEvent<HTMLSpanElement>) => {
+      if (!handleDragEvent(event, multiple)) {
+        return;
+      }
+      setDragOver(true);
+    },
+    [multiple]
+  );
+
+  const handleDragOver = useCallback(
+    (event: DragEvent) => handleDragEvent(event, multiple),
+    [multiple]
+  );
+
+  const handleDragLeave = useCallback((event: DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setDragOver(false);
+  }, []);
+
+  const handleDrop = useCallback(
+    (event: DragEvent) => {
+      event.stopPropagation();
+      event.preventDefault();
+      processFiles(event.dataTransfer.files).then((filesInfoEvent) => {
+        setFilesInfo(filesInfoEvent);
+        const newValue = filesInfoEvent.map((fileInfo) => fileInfo.dataURL);
+        if (multiple) {
+          onChange(newValue);
+        } else {
+          onChange(newValue[0]);
+        }
+      });
+    },
+    [multiple, onChange]
+  );
+
   return {
     handleChange,
+    handleClear,
+    dragOver,
+    droppableProps: {
+      onDragEnter: handleDragEnter,
+      onDragOver: handleDragOver,
+      onDragLeave: handleDragLeave,
+      onDrop: handleDrop,
+    },
     filesInfo,
   };
 }
--- src/widgets/FileWidget.tsx
+++ src/widgets/FileWidget.tsx
@@ -1,16 +1,80 @@
-// TODO FIXME Files
+// TODO FilesInfo to templates???
 import type { FC } from "react";
 import type { WidgetProps } from "@rjsf/utils";
-import { getTemplate } from "@rjsf/utils";
+import type { FilesInfoProps } from "../rjsf-core/useFileWidget";
+import { useFileWidget } from "../rjsf-core/useFileWidget";
+import clsx from "clsx";
+import { RemoveButton } from "../templates/IconButton";
+import { isSmallClass } from "../ui";
+import { BsPlus } from "@react-icons/all-files/bs/BsPlus";
+
+const FilesInfo: FC<FilesInfoProps> = ({ filesInfo, multiple }) => {
+  if (filesInfo.length === 0) {
+    const placeholder = multiple
+      ? "Choose or drop files."
+      : "Choose or drop a file.";
+    return <span className="placeholder">{placeholder}</span>;
+  }
+  if (filesInfo.length === 1) {
+    return <>1 file. ({filesInfo[0].name})</>;
+  }
+  return (
+    <>
+      {filesInfo.length} files. ({filesInfo.map((info) => info.name).join(", ")}
+      )
+    </>
+  );
+};
 
 const FileWidget: FC<WidgetProps> = (props) => {
-  const { options, registry } = props;
-  const BaseInputTemplate = getTemplate<"BaseInputTemplate">(
-    "BaseInputTemplate",
-    registry,
-    options
+  const { id, readonly, disabled, autofocus, multiple, options, formContext } =
+    props;
+  const { handleChange, handleClear, dragOver, droppableProps, filesInfo } =
+    useFileWidget(props);
+  const className = isSmallClass(formContext);
+  const disabledInput = disabled || readonly;
+  return (
+    <div className="field has-addons">
+      <div className="control is-expanded">
+        <div
+          className={clsx("input", className, dragOver && "is-success")}
+          {...droppableProps}
+        >
+          <FilesInfo filesInfo={filesInfo} />
+        </div>
+      </div>
+      <div className="control">
+        <input
+          id={id}
+          name={id}
+          type="file"
+          style={{ display: "none" }}
+          disabled={disabledInput}
+          onChange={handleChange}
+          value=""
+          autoFocus={autofocus}
+          multiple={multiple}
+          accept={options.accept ? String(options.accept) : undefined}
+        />
+        <label htmlFor={id}>
+          <a
+            className={clsx("button", className)}
+            title="Choose"
+            // @ts-expect-error: TS2322 bulma does not supported 'is-disabled'.
+            disabled={disabledInput}
+          >
+            <BsPlus />
+          </a>
+        </label>
+        <RemoveButton
+          iconType="block"
+          className={className}
+          disabled={disabledInput || filesInfo.length === 0}
+          onClick={handleClear}
+        />
+      </div>
+    </div>
   );
-  return <BaseInputTemplate {...props} type="file" />;
 };
 
 export default FileWidget;
