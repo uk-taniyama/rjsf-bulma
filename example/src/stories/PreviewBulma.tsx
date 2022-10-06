9aa298b5aaebc1e29a4592228901ef21df26f1a8
uk-taniyama <uk.taniyama@gmail.com>
2022-10-06 23:42:29 +0900

    update example.

--- example/src/stories/PreviewBulma.tsx
+++ example/src/stories/PreviewBulma.tsx
@@ -1,22 +1,67 @@
-import type { FC } from "react";
-import { useState } from "react";
+import type { FC, ReactElement } from "react";
+import { useEffect } from "react";
+import { useCallback, useState } from "react";
+import type { FilesInfoProps } from "rjsf-bulma";
 import Form, { createIsSmallUiSchema } from "rjsf-bulma";
 import validator from "@rjsf/validator-ajv6";
 import { getSample } from "./sample";
+import type { TemplatesType } from "@rjsf/utils";
+import clsx from "clsx";
 
 import "rjsf-bulma/scss/bulma.scss";
+
+const CustomFilesInfoTemplate: FC<FilesInfoProps> = (props) => {
+  return (
+    <pre
+      style={{
+        overflow: "scroll",
+        wordBreak: "break-all",
+        width: "100%",
+        maxWidth: "80vw",
+      }}
+    >
+      {JSON.stringify(props)}
+    </pre>
+  );
+};
+
+const templates: Partial<TemplatesType> = {
+  FilesInfoTemplate: CustomFilesInfoTemplate,
+};
 
 export interface PreviewProps {
   name: string;
 }
 
+function useToggleButton(id: string): [boolean, ReactElement] {
+  const [state, setState] = useState(false);
+  const handleClick = useCallback(() => setState((v) => !v), []);
+  return [
+    state,
+    <button
+      key={id}
+      id={id}
+      className={clsx("button is-small is-primary mr-1", state && "is-active")}
+      onClick={handleClick}
+    >
+      {id} {state ? "ON" : "off"}
+    </button>,
+  ];
+}
+
 const Preview: FC<PreviewProps> = ({ name }) => {
-  const [isSmall, setIsSmall] = useState(false);
+  const [isSmall, toggleIsSmall] = useToggleButton("isSmall");
+  const [dummy, setDummy] = useState(false);
+  const [customFilesInfo, toggleCustomFilesInfo] =
+    useToggleButton("customFilesInfo");
+  // NOTE FORCE RENDER!!!
+  useEffect(() => {
+    setDummy((v) => !v);
+  }, [customFilesInfo]);
   const header = (
     <div>
-      <button id="isSmall" onClick={() => setIsSmall((v) => !v)}>
-        {isSmall ? "isSmall" : "!isSmall"}
-      </button>
+      {toggleIsSmall}
+      {toggleCustomFilesInfo}
     </div>
   );
   const sample = getSample(name) as any;
@@ -34,8 +79,9 @@ const Preview: FC<PreviewProps> = ({ name }) => {
       <Form
         validator={validator}
         {...rest}
-        formContext={{ bulma: { isSmall } }}
+        formContext={{ bulma: { isSmall: dummy ? !isSmall : isSmall } }}
         uiSchema={isSmall ? createIsSmallUiSchema(uiSchema) : uiSchema}
+        templates={customFilesInfo ? templates : undefined}
       ></Form>
     </>
   );
