365a6d9ad70c2a5378e8f2e4897c28a4f760ac13
uk-taniyama <uk.taniyama@gmail.com>
2022-10-07 13:06:43 +0900

    処理をテンプレートに切り出し。

--- src/widgets/FileWidget.tsx
+++ src/widgets/FileWidget.tsx
@@ -1,12 +1,9 @@
-// TODO FilesInfo to templates???
 import type { FC } from "react";
+import { useCallback, useRef } from "react";
 import type { WidgetProps } from "@rjsf/utils";
-import { getTemplate, getUiOptions } from "@rjsf/utils";
-import { useFileWidget } from "../rjsf-core/useFileWidget";
 import clsx from "clsx";
-import { RemoveButton } from "../templates/IconButton";
+import { useFileWidget } from "../rjsf-core/useFileWidget";
 import { isSmallClass } from "../ui";
-import { BsPlus } from "@react-icons/all-files/bs/BsPlus";
 
 const FileWidget: FC<WidgetProps> = (props) => {
   const {
@@ -18,17 +15,21 @@ const FileWidget: FC<WidgetProps> = (props) => {
     options,
     formContext,
     registry,
-    uiSchema,
   } = props;
   const { handleChange, handleClear, dragOver, droppableProps, filesInfo } =
     useFileWidget(props);
+  const fileRef = useRef<HTMLInputElement>(null);
+  const handleChoose = useCallback(() => {
+    fileRef.current?.click();
+  }, [fileRef]);
+
+  const {
+    FilesInfoTemplate,
+    ButtonTemplates: { AddButton, RemoveButton },
+  } = registry.templates;
+
   const className = isSmallClass(formContext);
   const disabledInput = disabled || readonly;
-  const FilesInfoTemplate = getTemplate(
-    "FilesInfoTemplate",
-    registry,
-    getUiOptions(uiSchema)
-  );
   return (
     <div className="field has-addons">
       <div className="control is-expanded">
@@ -36,34 +37,32 @@ const FileWidget: FC<WidgetProps> = (props) => {
           className={clsx("input", className, dragOver && "is-success")}
           {...droppableProps}
         >
+          <input
+            ref={fileRef}
+            id={id}
+            name={id}
+            type="file"
+            style={{ display: "none" }}
+            disabled={disabledInput}
+            onChange={handleChange}
+            value=""
+            autoFocus={autofocus}
+            multiple={multiple}
+            accept={options.accept ? String(options.accept) : undefined}
+          />
           <FilesInfoTemplate filesInfo={filesInfo} multiple={multiple} />
         </div>
       </div>
       <div className="control">
-        <input
-          id={id}
-          name={id}
-          type="file"
-          style={{ display: "none" }}
+        <AddButton
+          title="Choose"
+          className={className}
           disabled={disabledInput}
-          onChange={handleChange}
-          value=""
-          autoFocus={autofocus}
-          multiple={multiple}
-          accept={options.accept ? String(options.accept) : undefined}
+          onClick={handleChoose}
         />
-        <label htmlFor={id}>
-          <a
-            className={clsx("button", className)}
-            title="Choose"
-            // @ts-expect-error: TS2322 bulma does not supported 'is-disabled'.
-            disabled={disabledInput}
-          >
-            <BsPlus />
-          </a>
-        </label>
+      </div>
+      <div className="control">
         <RemoveButton
-          iconType="block"
           className={className}
           disabled={disabledInput || filesInfo.length === 0}
           onClick={handleClear}
